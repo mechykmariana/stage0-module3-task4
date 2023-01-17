e3b5342fa50f3456c81d3741fa2effe205dd44cb
mariana_mechyk <mariana.mechyk@ecenta.com>
2023-01-18 00:34:09 +0200

    Types of variables

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,7 +2,8 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
-        int a, b;
+        int a = 0;
+        int b = 0;
         System.out.println((9 * a * a - 5 * b + 2 + a - 7)*((a + b - 4 * a * b)/2));
     }
 }
