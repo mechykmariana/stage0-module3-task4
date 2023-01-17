e9b6284c9fa74391e8e9062a31bee6d838ddf0c7
mariana_mechyk <mariana.mechyk@ecenta.com>
2023-01-18 00:31:27 +0200

    Types of variables

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int charC = 99;
+        int charH = 104;
+        int charA = 97;
+        int charR = 114;
     }
 }
